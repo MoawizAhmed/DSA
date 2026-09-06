4b4b601bc544877464dcc05ecd71b06fb41c9af3
MoawizAhmed <mob.iqamar12@gmail.com>
2026-09-06 13:33:14 +0500

    Mid preps '24 question 3'

--- DSAA/mid1.cpp
+++ DSAA/mid1.cpp
@@ -109,76 +109,135 @@ using namespace std;
 //     p.nextsong();
 //     return 0;
 // }
-class stack
-{
- private:
- char* c;
- int capacity;
- int size;
- public:
- stack()
- {
-    capacity=1;
-    c=new char[capacity];
-    size=0;
- }
- void push(char ch)
- {
-  if(size==capacity)
-  {
-    char* temp=new char[capacity*2];
-    capacity*=2;
-    for (int i = 0; i < size; i++)
-    {
-        temp[i]=c[i];
-    }
-      delete[] c;
-    c=temp;
-    c[size++]=ch;
-     return;
-    }
-    c[size++]=ch;
- }
- void pop()
- {
-    for (int i = 0; i < size-1; i++)
-    {
-        c[i]=c[i+1];
-    }
-    size--;
- }
- void reverse()
- {
-    int i = 0;
-    int j = size - 1;
+// class stack
+// {
+//  private:
+//  char* c;
+//  int capacity;
+//  int size;
+//  public:
+//  stack()
+//  {
+//     capacity=1;
+//     c=new char[capacity];
+//     size=0;
+//  }
+//  void push(char ch)
+//  {
+//   if(size==capacity)
+//   {
+//     char* temp=new char[capacity*2];
+//     capacity*=2;
+//     for (int i = 0; i < size; i++)
+//     {
+//         temp[i]=c[i];
+//     }
+//       delete[] c;
+//     c=temp;
+//     c[size++]=ch;
+//      return;
+//     }
+//     c[size++]=ch;
+//  }
+//  void pop()
+//  {
+//     for (int i = 0; i < size-1; i++)
+//     {
+//         c[i]=c[i+1];
+//     }
+//     size--;
+//  }
+//  void reverse()
+//  {
+//     int i = 0;
+//     int j = size - 1;
 
-    while(i < j)
-    {
-        char temp = c[i];
-        c[i] = c[j];
-        c[j] = temp;
+//     while(i < j)
+//     {
+//         char temp = c[i];
+//         c[i] = c[j];
+//         c[j] = temp;
 
-        i++;
-        j--;
-    }
- }
- void printt()
- {
-    for(int i=0;i<size;i++)
-    {
-        cout<<c[i];
-    }
- }
-};
-int main()
-{
-    stack s;
-    s.push('n');
-    s.push('i');
-    s.push('g');
-    s.push('g');
-    s.push('a');
-    s.reverse();
-    s.printt();
-    return 0;
-}
+//         i++;
+//         j--;
+//     }
+//  }
+//  void printt()
+//  {
+//     for(int i=0;i<size;i++)
+//     {
+//         cout<<c[i];
+//     }
+//  }
+// };
+// int main()
+// {
+//     stack s;
+//     s.push('n');
+//     s.push('i');
+//     s.push('g');
+//     s.push('g');
+//     s.push('a');
+//     s.reverse();
+//     s.printt();
+//     return 0;
+// }
+// class university
+// {
+//  private:
+//  int iD;
+//  double GPA;
+//  string name;
+//  int age;
+//  public:
+//  university(string name,int age,double gpa,int id)
+//  {
+//     this->age=age;
+//     this->GPA=gpa;
+//     this->name=name;
+//     this->iD=id;
+//  }
+//  university(){}
+//  university(university *u)
+//  {
+//   this->GPA=u->GPA;
+//   this->age=u->age;
+//   this->iD=u->iD;
+//   this->name=u->name;
+//  }
+//  university& operator=(university *u)
+//  {
+//   university temp;
+//   temp.age=u->age;
+//   temp.name=u->name;
+//   temp.iD=u->iD;
+//   temp.GPA=u->GPA;
+//   return temp;
+//  }
+//  float getgpa(){return GPA;}
+// };
+// int main()
+// { 
+//     const int n=5;
+//     university s1("moawiz",18,3.1,1);
+//     university s2("ali",19,2.6,2);
+//     university s3("hamza",20,2.3,3);
+//     university s4("arsal",20,2.9,4);
+//     university s5("wajih",21,2.6,5);
+//     university u[n]={s1,s2,s3,s4,s5};
+
+//     for (int i = 0; i < n-1; i++)
+//     {
+//     for (int j = 0; j < n-i-1; j++)
+//     {
+//         if(u[j].getgpa()<u[j+1].getgpa())
+//         {
+//             university temp=u[j];
+//             u[j]=u[j+1];
+//             u[j+1]=temp;
+//         }
+//     }
+//     }
+    
+//     return 0;
+// }
